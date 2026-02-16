c51e18f5b19b9adc9ad3989b5ab3ef6648a1fe0b
Sophia Fan <sophiaxfan@gmail.com>
2026-02-16 13:25:57 -0800

    2nd update to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Sophia Fan" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("Hi! I'm Sophia Fan, a sophomore from California. I love reading fantasy books and listening to music.")
 if __name__ == "__main__":
     main()
